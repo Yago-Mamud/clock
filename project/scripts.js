03dc97e4e4e1b419c6319081e54108984a7b9fc9
Yago <yagomamud11@gmail.com>
2026-06-03 11:19:38 -0300

    stopwatch started

--- project/scripts.js
+++ project/scripts.js
@@ -1,6 +1,23 @@
-const horario = document.getElementById("horario");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
 const timer = setInterval(
 	function() {
 		const teste = new Date();
-		horario.innerText = teste.getSeconds();
+		hours.innerText = teste.getHours();
+		minutes.innerText = teste.getMinutes();
+		seconds.innerText = teste.getSeconds();
 	}, 1000);
+	
+//
+const sw = document.getElementById("stopwatch");
+function buttonHandler() {
+	const stopwatch = setInterval(
+		function() {
+			let secondCounter;
+			secondCounter += 1;
+			sw.innerText = secondCounter;
+		}, 1000);
+}
+const start = document.getElementById("start");
+start.addEventListener("click", buttonHandler);
