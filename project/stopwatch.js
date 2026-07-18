a36d19b5e9b4e3ae48cf647e3b05d6f93d1ce146
Yago <yagomamud11@gmail.com>
2026-07-18 11:11:16 -0300

    stopwatch tab change glitch fixed. stopwatch rescripted

--- project/stopwatch.js
+++ project/stopwatch.js
@@ -1,3 +1,4 @@
+//STOPWATCH HANDLER
 //Main Button
 const start = document.getElementById("start");
 
@@ -14,32 +15,34 @@ var counterM = 0;
 //Logic that decides wheter the stopwatch is active or not
 let isTimerActive = false;
 
+//Time handling variables
+//Starting time
+let startTimeCounter = 0;
+//How much time has passed since the activation of the stopwatch
+let timePassed = 0;
+
 //Stopwatch variable has to be outside so it wont mess with the scope
 let stopwatch
 
 //Handles the stopwatch
 start.addEventListener("click", () => {
 	if (!isTimerActive) {
+		//Handles whether the stopwatch is active or not
 		isTimerActive = !isTimerActive;
-		stopwatch = setInterval( () => {
-			counterMS += 5;
-			//Manages minutes
-			if (counterS>59) {
-				counterS = 0;
-				counterM+=1
-				stopwatchM.innerText = String(counterM).padStart(2,"0");
-				//seconds updater has to be here or it will exhibit the "60" in the stopwatch
-				stopwatchS.innerText = String(counterS).padStart(2,"0");
-			}
-			//Manages seconds
-			if (counterMS>=1000) {
-				counterMS = 0;
-				counterS+=1
-				stopwatchS.innerText = String(counterS).padStart(2,"0");
-			}
-			//Manages milliseconds
-			stopwatchMS.innerText = String(counterMS).padStart(3,"0");
-		}, 5);
+		//Handles the stopwatch resume
+		startTimeCounter = Date.now() - timePassed
+		stopwatch = setInterval( () => { 
+		
+			timePassed = Date.now() - startTimeCounter
+			
+			const minutes = Math.floor((timePassed/1000/60)%60)
+			const seconds = Math.floor((timePassed/1000)%60)
+			const milliseconds = Math.floor(timePassed % 1000)
+			
+			stopwatchM.innerText = String(minutes).padStart(2,"0");
+			stopwatchS.innerText = String(seconds).padStart(2,"0");
+			stopwatchMS.innerText = String(milliseconds).padStart(3,"0");
+		}, 10);
 	//Manages the stop button
 		start.innerHTML = "Stop"
 	} else {
@@ -47,4 +50,18 @@ start.addEventListener("click", () => {
 		clearInterval(stopwatch);
 		start.innerHTML = "Start"
 	}
+});
+
+//LAP HANDLER
+//Main Button
+const lap = document.getElementById("lap");
+
+//HTML elements
+
+const lapContainer = document.getElementById("lapContainer");
+
+lap.addEventListener("click", () => {
+	const newMark = document.createElement("p");
+	newMark.textContent = stopwatchM.innerText + ":" + stopwatchS.innerText + ":" + stopwatchMS.innerText;
+	lapContainer.appendChild(newMark);
 });
