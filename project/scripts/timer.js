4194dc67ef1aa5bdc4709d5532da5efec3e8ce88
Yago <yagomamud11@gmail.com>
2026-08-08 17:29:10 -0300

    timer is now working, although very glitched

--- project/scripts/timer.js
+++ project/scripts/timer.js
@@ -1,6 +1,7 @@
 const timerButton = document.getElementById("timerButton");
 
 const timerSecond = document.getElementById("timerSecond");
+const timerMinute = document.getElementById("timerMinute");
 
 let timerActivationHandler = false;
 let timerHandler;
@@ -8,12 +9,22 @@ let timerHandler;
 timerButton.addEventListener("click", () => {
 	if (!timerActivationHandler) {
 		//Handles whether the timer is active or not
-		timerActivationHandler = !timerActivationHandler;
+		timerActivationHandler = !timerActivationHandler;		
 		//
-		let currentSecond = Number(timerSecond.innerHTML);
 		timerHandler = setInterval( () => {
-			currentSecond -= 1;
-			timerSecond.innerHTML = currentSecond
+			//
+			let currentSecond = Number(timerSecond.innerHTML);
+			let currentMinute = Number(timerMinute.innerHTML);
+			//
+			if (currentSecond >= 1) {
+				currentSecond -= 1;
+			} else {
+				currentMinute -= 1
+				currentSecond = 59;
+			}
+			timerMinute.innerHTML = currentMinute;
+			timerSecond.innerHTML = currentSecond;
+			//
 		}, 1000);
 	//Manages the stop button
 		timerButton.innerHTML = "Stop"
