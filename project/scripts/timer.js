ec1aaf8988b2d35c72c8e32e366537595871a2bf
Yago <yagomamud11@gmail.com>
2026-08-15 13:11:10 -0300

    safety backup

--- project/scripts/timer.js
+++ project/scripts/timer.js
@@ -16,21 +16,41 @@ timerButton.addEventListener("click", () => {
 			let currentSecond = Number(timerSecond.innerHTML);
 			let currentMinute = Number(timerMinute.innerHTML);
 			//
+			if (currentMinute === 0 && currentSecond === 0) {
+				stopHandler();
+				return;
+			}
+			//
 			if (currentSecond >= 1) {
 				currentSecond -= 1;
 			} else {
 				currentMinute -= 1
 				currentSecond = 59;
 			}
-			timerMinute.innerHTML = currentMinute;
-			timerSecond.innerHTML = currentSecond;
+			timerMinute.innerHTML = String(currentMinute).padStart(2,"0");
+			timerSecond.innerHTML = String(currentSecond).padStart(2,"0");
 			//
 		}, 1000);
 	//Manages the stop button
 		timerButton.innerHTML = "Stop"
 	} else {
-		timerActivationHandler = !timerActivationHandler;
-		clearInterval(timerHandler);
-		timerButton.innerHTML = "Start"
+		stopHandler();
 	}
+});
+
+function stopHandler() {
+	timerActivationHandler = !timerActivationHandler;
+	clearInterval(timerHandler);
+	timerButton.innerHTML = "Start"
+}
+
+//Time adder handler
+const timeButtons = document.querySelectorAll(".timeAdder");
+timeButtons.forEach((tButt) => {
+	tButt.addEventListener("click", () => {
+		let buttonValue = Number(tButt.getAttribute("data-timeValue"));
+		let currentTimeValue = Number(timerSecond.innerHTML);
+		let timeToAdd = currentTimeValue + buttonValue;
+		timerSecond.innerHTML = String(timeToAdd).padStart(2,"0");
+	});
 });
